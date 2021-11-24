e179cbffde262cb345afad7f84c0e706fb2b8594
Deividgp <dayd4@outlook.com>
2021-11-24 10:31:08 +0100

    Initial commit

---
+++ Example1/Data/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Example1.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public IConfiguration Configuration { get; }
+
+        public DbSet<User> Users { get; set; } = null!;
+
+        public AppDBContext(IConfiguration configuration, DbContextOptions<AppDBContext> options) : base(options)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
---
+++ Example1/Data/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Example1.Data
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Name { get; set; }
+    }
+}
